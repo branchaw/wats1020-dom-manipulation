94aa7e78442768d631503741a38c4070ac02d8ce
branchaw <branchaw@gmail.com>
2016-01-29 19:57:00 -0500

    Completed basic requirements.

--- js/main.js
+++ js/main.js
@@ -13,35 +13,66 @@ $( document ).ready(function() {
         greatest: 0,
         total: 0
     };
-    // Place all your Javascript code inside this "document ready" function so
-    // it does not run until the DOM is ready for Javascript manipulation.
+    
+    // Created a function to listen for clicks on the "login" button. On click of the login button form slides up.
 
-    // TODO: Create a function to listen for clicks on the "login" button.
-    //      1. When a user clicks the "login" button, hide the login
-    //          form elements on the page.
-    //      2. Fill the user's first and last name into `div.user-info`.
-    //      (NOTE: You do not have to perform any validation on the data as
-    //          a base requirement.)
-
-
-    // TODO: Create a function to listen for clicks on all the "View Details"
+     $('#login').on('click', function() {
+        $('.navbar-form').slideUp(400);
+        $('.navbar-collapse').find('.user-info').delay(100).slideDown(500);       
+      });
+  
+  //Created another function on click of logout button to bring back the login form.
+  
+      $('#logout').on('click', function() {
+        $('.navbar-collapse').find('.user-info').hide();
+        $('.navbar-form').slideDown(500);
+      });
+      
+  
+    // Created a function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
     // the content contained in the elements with the class "details" in the
     // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
 
-    // TODO: Create a function that listens for clicks on the voting buttons and
+    $('.view-details').on('click', function(event){
+      console.log(event);
+      var targetElement = event.target;
+      var container = targetElement.parentElement.parentElement;
+      $(container).find('.details').each(function(index, el){
+        if ($(el).is(':visible')){
+          $(el).fadeOut();
+          targetElement.innerText = "View Details"
+        } else {
+          $(el).fadeIn();
+          targetElement.innerText = "Hide Details"
+        }
+      });
+    });
+  
+    // Created a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
     // then determines the updated vote breakdown to adjust the progress bars.
-    //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+   
+    var updateBars = function(voteCounts) {
+    var greatBar=$('.great-progress');
+    var greatestBar=$('.greatest-progress');
+    var greatWidth=voteCounts.great/voteCounts.total;
+    var greatestWidth=voteCounts.greatest/voteCounts.total;
+    greatBar.css('width', greatWidth*100 + '%');
+    greatestBar.css('width', greatestWidth*100 + '%');
+  }
+   
+  $('.vote').on('click', function(event) {
+      var button=$(event.target);
+      if(button.data('vote') == 'great') {
+        voteCounts.great += 1; 
+      } else if(button.data('vote') == 'greatest') {
+          voteCounts.greatest += 1;
+        }
+        voteCounts.total +=1;
+        updateBars(voteCounts);
+    
+    });  
+  
 
 });
